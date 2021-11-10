2fce0bf2508e533dd5b7cf584d40f2bb21968031
PaleoPietn <lukrassful@gmail.com>
2021-11-10 10:17:33 +0100

    cleaning up and adding timestamps

--- src/exploration.R
+++ src/exploration.R
@@ -9,7 +9,7 @@ library(tidyverse)
 data <- fread("./data/clean/sample_bavaria.csv", sep = ",", header = TRUE)
 gc()
 
-# analysis ------------------------------------------------------------------------------------
+# exploration ------------------------------------------------------------------------------------
 
 smaller <- data %>% 
   sample_n(50000) %>% 
@@ -26,25 +26,25 @@ crop <- data %>%
 
 # crop sequence -----------------------------------------------------------
 
-n = 16*100
+n = 16*1000
 
 sliced_data <- data %>% 
   select(OBJECTID, Year, crtype) %>% 
   arrange(OBJECTID) %>% 
-  slice(1:n) %>% 
+  slice(1:n) %>%
   as.data.table()
 gc()
 
 crop_sequences <- sliced_data %>% 
-  pivot_wider(names_from = Year, values_from=crtype, values_fill = 0, values_fn = list(. = mean)) %>% 
+  pivot_wider(names_from = Year, values_from=crtype, values_fn = list(. = mean)) %>% 
   as.data.table()
 rm(sliced_data,data)
 gc()
 
 # most common n=2 crop sequence
-
-list_consecutives <- function(y){
-  res <- lapply(2, function(a) sapply(1:a, function(x) y[(0 + x):(length(y) - a + x)])) %>% 
+k = 2
+find_k_consecutives <- function(y, k){
+  res <- lapply(k, function(a) sapply(1:a, function(x) y[(0 + x):(length(y) - a + x)])) %>% 
     lapply(as.data.frame) %>% 
     setNames(sapply(2:(length(.) + 1), function(a) paste0("Consecutive", a)))
   return(res)
@@ -54,26 +54,30 @@ find_most_freq <- function(z) {
   lapply(1:length(z), function(x) (as.data.frame(table(do.call(
     paste, z[[x]])), stringsAsFactors = F) %>% 
       dplyr::mutate(length = nchar(Var1)) %>% 
-      dplyr::filter(#length == max(length) & 
-        Freq == max(Freq) & Freq > 1)) ) %>% 
+      dplyr::filter(Freq == max(Freq) & Freq > 0)) ) %>% 
     .[which(sapply(., nrow) > 0)] %>% 
-    dplyr::bind_rows() %>% 
-    dplyr::filter(Freq == max(Freq)) %>% 
-    dplyr::filter(length == max(length)) %>% 
-    dplyr::rename(Sequence = Var1) %>% 
+    dplyr::bind_rows() %>%
+    dplyr::filter(Freq == max(Freq)) %>%
+    dplyr::filter(length == max(length)) %>%
+    dplyr::rename(Sequence = Var1) %>%
     dplyr::select(-length)
 }
 
-get_most_frequent_sequence <- function(vec){
-  l <- list_consecutives(vec)
+get_most_frequent_sequence <- function(vec,k=2){
+  l <- find_k_consecutives(as.character(vec),k)
   res <- find_most_freq(l)
   
   return(res)
 }
 
-test = get_most_frequent_sequence(as.character(crop_sequences[1,-1]))
-for(i in c(2:n/16)){
-  t <- as.character(crop_sequences[i,-1])
-  test <- test %>% add_row(get_most_frequent_sequence(as.character(t)))
+start_time <- Sys.time()
+most_freq = get_most_frequent_sequence((crop_sequences[1,-1]),k)
+for(i in c(2:nrow(crop_sequences))){
+  temp <- (crop_sequences[i,-1])
+  most_freq <- most_freq %>% add_row(get_most_frequent_sequence((temp),k))
 }
+end_time <- Sys.time()
+print(end_time-start_time)
+which(table(most_freq$Sequence) == max(table(most_freq$Sequence)))
+
 
