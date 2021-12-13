be1c5fe46822017fa519e87643dc6dc779eec265
PaleoPietn <lukrassful@gmail.com>
2021-12-13 17:22:53 +0100

    add visualization to model results

--- src/model.R
+++ src/model.R
@@ -9,22 +9,28 @@ library(caret)
 # read data -----------------------------------------------------------------------------------
 
 # data <- fread("./data/clean/sample_bavaria.csv", sep = ",", header = TRUE)
-data <- fread("./data/clean/sample206934.csv", sep = ",", header = TRUE)
+data <- fread("./data/clean/sample_206934.csv", sep = ",", header = TRUE)
 
 data$State <- as.factor(data$State)
 data$X <- as.numeric(data$X)
 data$Y <- as.numeric(data$Y)
 data$Year <- as.factor(data$Year)
 data$CType <- as.factor(data$CType)
+data$PCType <- as.factor(data$PCType)
+data$PPCType <- as.factor(data$PPCType)
 data$SType <- as.factor(data$SType)
 data$SElev <- as.numeric(data$SElev)
 
+data <- data %>% drop_na()
+
 # split data ----------------------------------------------------------------------------------
 
-split_data <- function(dataset, test_size = 2) {
+split_data <- function(dataset) {
+  dataset <- dataset %>% select(-OBJECTID) %>% as.data.table()
   years <- unique(dataset$Year)
-  tail <- years[(length(years)-test_size+1):length(years)]
-  head <- years[1:(length(years)-test_size)]
+  tail <- years[(length(years)-2):(length(years)-1)]
+  head <- years[1:(length(years)-2)]
+  rest <- years[length(years)]
   
   train <- dataset %>% 
     filter(Year %in% head) %>% 
@@ -34,12 +40,17 @@ split_data <- function(dataset, test_size = 2) {
     filter(Year %in% tail) %>% 
     as.data.table()
   
-  return(list(train,test))
+  eval <- dataset %>% 
+    filter(Year %in% rest) %>% 
+    as.data.table()
+  
+  return(list(train,test,eval))
 }
 
 split <- split_data(data)
 train_data <- split[[1]]
 test_data <- split[[2]]
+eval_data <- split[[3]]
 
 # clean workplace -----------------------------------------------------------------------------
 
@@ -48,7 +59,7 @@ gc()
 
 # model ---------------------------------------------------------------------------------------
 
-res <- ranger(CType ~ ., data = train_data, 
+res <- ranger(CType ~ ., data = train_data,
               importance="impurity",
               oob.error = TRUE,
               seed = 187,
@@ -58,4 +69,12 @@ res <- ranger(CType ~ ., data = train_data,
 
 pred <- predict(res, data = test_data)
 
-confusionMatrix(pred$predictions ,test_data$CType)
+gc()
+
+mat <- confusionMatrix(pred$predictions ,test_data$CType)
+mat$table %>% confusionMatrix()
+
+plot(res$predictions, las = 2)
+
+imp <- res$variable.importance
+barplot(imp[order(imp, decreasing = TRUE)], las = 2)
